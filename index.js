1c4f151b5e7dc467aa9a45d3dc6d82756bbfe521
c_melchor <cah.melchor@gmail.com>
2021-01-26 08:12:46 -0800

    added line after last line of code in index

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require("./api/server");
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
+  console.log(`Server listening on port ${port}`);
+});
